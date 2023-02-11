d021811c7a648f8e8afd9cfcd1010aaf5f3da070
Vaibhav Bali <vibsbali@gmail.com>
2023-02-11 11:31:21 +1100

    using types and reading fieldInfo

--- Examples/Examples/Program.cs
+++ Examples/Examples/Program.cs
@@ -1,10 +1,46 @@
-﻿namespace Examples
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace Examples
 {
+    class BaseClass
+    {
+        public int BaseField = 0;
+    }
+    class DerivedClass : BaseClass
+    {
+        public int DerivedField = 0;
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var bc = new BaseClass();
+            var dc = new DerivedClass();
+
+            var bca = new BaseClass[] {bc, dc};
+
+            foreach (var instance in bca)
+            {
+                var t = instance.GetType();
+                Console.WriteLine($"Object type : {t.Name}");
+                
+                FieldInfo[] fi = t.GetFields();
+                foreach (var fieldInfo in fi)
+                {
+                    Console.WriteLine($"    Field : {fieldInfo.Name}");
+                }
+
+                Console.WriteLine();
+            }
+
+            Type tbc = typeof(DerivedClass); // Get the type.
+            Console.WriteLine($"Object type : {tbc.Name}");
+            FieldInfo[] fiUsingTypes = tbc.GetFields();
+            foreach (var f in fiUsingTypes)
+            {
+                Console.WriteLine($" Field : {f.Name}");
+            }
         }
     }
 }
